233403dee24031ca23541610d87f32493b4e6074
otobakataro <vancyroletaka@gmail.com>
2023-06-04 00:04:43 +0900

    add httphandler

--- internal/server/http.go
+++ internal/server/http.go
@@ -67,3 +67,31 @@ func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	var req ConsumeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	record, err := s.Log.Read(req.Offset)
+	if err == ErrOffsetNotFound {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := ConsumeResponse{Record: record}
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
